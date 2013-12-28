ca4c8f51e7630553d1c89afc65147ad761c4c4b7
Asoul Yang <b00902036@ntu.edu.tw>
2013-12-28 21:24:37 +0800

    new 2

--- code/getNGrams.py
+++ code/getNGrams.py
@@ -3,8 +3,8 @@ import collections
 def find_ngrams(input_list, n):
 	return zip(*[input_list[i:] for i in range(n)])
 
-f = open('../feature/9knife/1.txt', 'r')
-fw = open('../ngram/9knife/1.txt', 'w')
+f = open('../feature/9knife/2.txt', 'r')
+fw = open('../ngram/9knife/2.txt', 'w')
 
 total_grams = dict()
 
@@ -21,4 +21,4 @@ for line in f:
 tmp = total_grams.items()
 tmp.sort(key=lambda x:x[1], reverse=True)
 for t in tmp:
-	fw.write(str(t[0]) + '\t' + str(t[1]) + '\n')
+	fw.write(str(t[0]) + ' ' + str(t[1]) + '\n')
