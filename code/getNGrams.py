8dede0496753e3afaed75f7c2b571c89962bf0ed
Asoul Yang <b00902036@ntu.edu.tw>
2013-12-28 21:21:43 +0800

    @W@

--- code/getNGrams.py
+++ code/getNGrams.py
@@ -3,20 +3,22 @@ import collections
 def find_ngrams(input_list, n):
 	return zip(*[input_list[i:] for i in range(n)])
 
-d = collections.defaultdict(int)
-
 f = open('../feature/9knife/1.txt', 'r')
 fw = open('../ngram/9knife/1.txt', 'w')
+
+total_grams = dict()
 
 for line in f:
 	token = eval(line)
 	grams = find_ngrams(token, 3)
 
 	for gram in grams:
-		d[gram] += 1
+		if gram not in total_grams:
+			total_grams[gram] = 0
+		else:
+			total_grams[gram] += 1
 
-# total_grams.sort()
-
-print len(d)
-for gram in d:
-	print >> fw, gram
+tmp = total_grams.items()
+tmp.sort(key=lambda x:x[1], reverse=True)
+for t in tmp:
+	fw.write(str(t[0]) + '\t' + str(t[1]) + '\n')
