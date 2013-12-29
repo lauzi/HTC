92087e5109006a8548e392c3dd0b38584e5be164
Asoul Yang <b00902036@ntu.edu.tw>
2013-12-29 20:08:30 +0800

    all feature have done

--- code/getSynt.py
+++ code/getSynt.py
@@ -17,14 +17,13 @@ dir_num = [5, 24, 2]
 for dir_i in range(3):
 	print "doing ... ", dir_name[dir_i]
 	for file_i in range(1, dir_num[dir_i] + 1):
-		print "fomatting in file ", dir_name, file_i
+		print "now in file ", dir_name[file_i]
 		f = open('../format/' + dir_name[dir_i] + '/'+ str(file_i) + '.txt', 'r')
-		f = open('../feature/' + dir_name[dir_i] + '/'+ str(file_i) + '.txt', 'w')
+		fw = open('../feature/' + dir_name[dir_i] + '/'+ str(file_i) + '.txt', 'w')
 		count = 0
 		for line in f:
 			count += 1
-			if count % 100 == 0: print "working...", count
-				print "working ... file", file_i, " .. line ", count
+			if count % 100 == 0: print "working ... file", file_i, " .. line ", count
 			tokens = map(lambda x:x.flag, ps.cut(unicode(line.strip(), "utf8")))
 			# cut the line into tokens and use its flags
 			for token in tokens:
