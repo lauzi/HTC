94adcf29df4b4cc2efbc3cc0e5da5fdc104c7091
poao899 <poao899@bitbucket.org>
2013-12-29 18:11:02 +0800

    [Modified] remove all 標點

--- code/formatter.py
+++ code/formatter.py
@@ -3,10 +3,17 @@
 import codecs
 import re
 
-for file_i in range(1,6):
+def clean_str(str, invalid=u'，：；、…'):
+#    print ">>> get" + str
+    for ch in invalid:
+        str = str.replace(ch, "")
+#    print "<<< ret" + str
+    return str
 
-    fin = codecs.open('../data/cityup/'+ str(file_i) + '.txt', 'r', encoding='utf8')
-    fout = codecs.open('../format/cityup/' + str(file_i) + '.txt', 'w', encoding='utf8')
+for file_i in range(1,2):
+
+    fin = codecs.open('../data/love0/'+ str(file_i) + '.txt', 'r', encoding='utf8')
+    fout = codecs.open('../format/love0/' + str(file_i) + '.txt', 'w', encoding='utf8')
 
 #    fin = codecs.open('in.txt', 'r', encoding='utf8')
 #    fout = codecs.open('out.txt', 'w', encoding='utf8')
@@ -15,7 +22,7 @@ for file_i in range(1,6):
     rest_str = ""
     for line in fin:
         count += 1
-        inp = line.strip(u'“”﹝﹞「」”＂')
+        inp = clean_str(line, u'“”﹝﹞「」”＂『』')
         inp = inp.strip()
         now_lst = re.findall(u'[^。？！]*[。？！]?', inp)
         #   print len(now_lst)
@@ -23,9 +30,9 @@ for file_i in range(1,6):
             if len(now_str) > 0:
                 flag = (now_str[-1] in u'。？！')
                 if flag:
-                    print >> fout, (rest_str + now_str)
+                    print >> fout, (rest_str + clean_str(now_str[:-1]))
                     rest_str = ""
                 elif now_str[-1] in u'，：；、…':
-                    rest_str = rest_str + now_str
+                    rest_str = rest_str + clean_str(now_str[:-1])
                 else:
                     rest_str = ""
