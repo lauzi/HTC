4c4db75b2e628a9644cf57b308579b297266bad8
Asoul Yang <b00902036@ntu.edu.tw>
2013-12-29 19:15:56 +0800

    auto start

--- code/formatter.py
+++ code/formatter.py
@@ -10,29 +10,33 @@ def clean_str(str, invalid=u'，：；、…'):
 #    print "<<< ret" + str
     return str
 
-for file_i in range(1,2):
+dir_name = ["cityup", "9knife", "love0"]
+dir_num = [5, 24, 2]
 
-    fin = codecs.open('../data/cityup/'+ str(file_i) + '.txt', 'r', encoding='utf8')
-    fout = codecs.open('../format/cityup/' + str(file_i) + '.txt', 'w', encoding='utf8')
+for dir_i in range(3):
+    for file_i in range(1, dir_num[dir_i] + 1):
 
-#    fin = codecs.open('in.txt', 'r', encoding='utf8')
-#    fout = codecs.open('out.txt', 'w', encoding='utf8')
+        fin = codecs.open('../data/' + dir_name[dir_i] + '/'+ str(file_i) + '.txt', 'r', encoding='utf8')
+        fout = codecs.open('../format/' + dir_name[dir_i] + '/' + str(file_i) + '.txt', 'w', encoding='utf8')
 
-    count = 0
-    rest_str = ""
-    for line in fin:
-        count += 1
-        inp = clean_str(line, u'“”﹝﹞「」”＂『』')
-        inp = inp.strip()
-        now_lst = re.findall(u'[^。？！]*[。？！]?', inp)
-        #   print len(now_lst)
-        for now_str in now_lst:
-            if len(now_str) > 0:
-                flag = (now_str[-1] in u'。？！')
-                if flag:
-                    print >> fout, (rest_str + clean_str(now_str[:-1]))
-                    rest_str = ""
-                elif now_str[-1] in u'，：；、…':
-                    rest_str = rest_str + clean_str(now_str[:-1])
-                else:
-                    rest_str = ""
+    #    fin = codecs.open('in.txt', 'r', encoding='utf8')
+    #    fout = codecs.open('out.txt', 'w', encoding='utf8')
+
+        count = 0
+        rest_str = ""
+        for line in fin:
+            count += 1
+            inp = clean_str(line, u'“”﹝﹞「」”＂『』《》—')
+            inp = inp.strip()
+            now_lst = re.findall(u'[^。？！]*[。？！]?', inp)
+            #   print len(now_lst)
+            for now_str in now_lst:
+                if len(now_str) > 0:
+                    flag = (now_str[-1] in u'。？！')
+                    if flag:
+                        print >> fout, (rest_str + clean_str(now_str[:-1]))
+                        rest_str = ""
+                    elif now_str[-1] in u'，：；、…':
+                        rest_str = rest_str + clean_str(now_str[:-1])
+                    else:
+                        rest_str = ""
--- code/getSynt.py
+++ code/getSynt.py
@@ -12,19 +12,21 @@ def freq_dict(ngrams_list):
 		d[x] += 1
 	return d
 
-for file_i in range(1,2):
-	print "fomatting in file ", file_i
-
-	f = open('../format/cityup/'+ str(file_i) + '.txt', 'r')
-	fw = open('../feature/cityup/' + str(file_i) + '.txt', 'w')
-	count = 0
-	for line in f:
-		count += 1
-		if count % 10 == 0:
-			break
-			print "working ... file", file_i, " .. line ", count
-		tokens = map(lambda x:x.flag, ps.cut(unicode(line.strip(), "utf8")))
-		# cut the line into tokens and use its flags
-		for token in tokens:
-			fw.write(token + ' ')
-		fw.write('\n')
+dir_name = ["cityup", "9knife", "love0"]
+dir_num = [5, 24, 2]
+for dir_i in range(3):
+	for file_i in range(1, dir_num[dir_i] + 1):
+		print "fomatting in file ", dir_name, file_i
+		f = open('../format/' + dir_name[dir_i] + '/'+ str(file_i) + '.txt', 'r')
+		f = open('../feature/' + dir_name[dir_i] + '/'+ str(file_i) + '.txt', 'w')
+		count = 0
+		for line in f:
+			count += 1
+			if count % 10 == 0:
+				break
+				print "working ... file", file_i, " .. line ", count
+			tokens = map(lambda x:x.flag, ps.cut(unicode(line.strip(), "utf8")))
+			# cut the line into tokens and use its flags
+			for token in tokens:
+				fw.write(token + ' ')
+			fw.write('\n')
