804171c3ae93f0c475b9b5a58d305ea0feff11d2
Asoul Yang <b00902036@ntu.edu.tw>
2014-01-02 01:39:13 +0800

    add some comment

--- code/getBadWords.py
+++ code/getBadWords.py
@@ -1,5 +1,7 @@
 import codecs
 
+# getBadWords.py simply count all word's frequency and output to ../badword/
+
 authors = {'9knife' : 24, 'cityup' : 5, 'love0' : 2}
 
 for (author, num) in authors.items():
@@ -10,14 +12,13 @@ for (author, num) in authors.items():
 		fw = codecs.open('../badword/'+author+'_'+str(book_num)+'.txt', 'w', encoding='utf8')
 		words = dict()
 		for line in f:
-			for token in line.strip():
+			for token in line.strip():# remove the newline character
 				if token in words:
 					words[token] += 1
 				else:
 					words[token] = 1
 		
 		tmp = words.items()
-		tmp.sort(key=lambda x:x[1], reverse=True)
+		tmp.sort(key=lambda x:x[1], reverse=True)# sort by the word's frequency (higher -> lower)
 		for i in tmp:
-			#print i[0], " ", i[1]
 			print >> fw, i[0], " ", i[1]
