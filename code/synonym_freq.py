4c94d8da1072cd0df13d91cb3aa4f26a00b4b47d
LauZi <st61112@gmail.com>
2014-01-07 04:13:04 +0900

    Add syn_freq.query

--- code/synonym_freq.py
+++ code/synonym_freq.py
@@ -1,11 +1,25 @@
+# A library for word swapping.
+#
+# A word's preference = syn_weight * (synonym_freq.freq_offset + actual word freq)
+#
+# Example:
+# import synonym_freq
+# import word_freq
+# import synonyms
+# (dick, invdick) = synonyms.read_wordbank('../data/wordbank.txt')
+# freq_dick = word_freq.load_file(file_name)
+# candidates = synonym_freq.query('duck', dick, invdick, freq_dick)
+# print candicates
+
 # -*- coding: utf-8 -*-
 
 import codecs
 import sys
-import os
 
 import word_freq
 import synonyms
+
+freq_offset = 3
 
 def enum_file_names(dirs = None):
     if dirs is None:
@@ -16,6 +30,27 @@ def enum_file_names(dirs = None):
     return ['%s/%s.txt' % (fname, fidx)
             for fname, fidxs in dirs
             for fidx in range(1, fidxs+1)]
+
+def reweight(dick):
+    """Reweights a DICKTIONARY so that its values' sum will be 1"""
+
+    total = sum(dick.items())
+    return {key: float(val)/total for key, val in dick}
+
+def query(word, dick, invdick, freqdick):
+    """Returns [(syn, prob.)] where `syn` is a synonym of `word` and
+    `prob.` is its weight, i.e. how much we prefer it."""
+    global freq_offset
+
+    # sim for similarities
+    syn_sims = reweight(synonyms.query_word(word, dick, invdick))
+
+    # we should give any word a chance
+    res_freqs = reweight({key: val * (freqdick.get(key, 0) + freq_offset)
+                          for key, val in syn_sims})
+
+    return sorted(res_freqs.iteritems(), lambda x: x[1], reversed=True)
+    return res_freq.items() # comment prev. line if we don't need to sort the result
 
 if __name__ == '__main__':
     (dick, invdick) = synonyms.read_wordbank('../data/wordbank.txt')
@@ -29,14 +64,14 @@ if __name__ == '__main__':
     def count(file_name, output_file = sys.stdout):
         freq_dick = word_freq.load_file(file_name)
 
-        dick = {}
+        syn_freq_dick = {}
         for word in words:
             if word in freq_dick:
-                dick[word] = freq_dick[word]
+                syn_freq_dick[word] = freq_dick[word]
 
         print >>output_file, 'File name:', file_name
-        total = sum(dick.values())
-        for word, freq in sorted(dick.items(), key=lambda x: x[1], reverse=True)[:30]:
+        total = sum(syn_freq_dick.values())
+        for word, freq in sorted(syn_freq_dick.iteritems(), key=lambda x: x[1], reverse=True)[:30]:
             print >>output_file, word, '%d/%d =' % (freq, total), float(freq) / total
 
     with codecs.open('../wordfreq/synonym_freqs.txt', 'w', encoding='utf8') as File:
