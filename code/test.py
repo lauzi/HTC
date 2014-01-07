47ac749c6f5d4b785eff6e89403b630937ee4bc3
LauZi <st61112@gmail.com>
2014-01-07 17:42:29 +0800

    Add test.py for jieba

--- code/test.py
+++ code/test.py
@@ -1,4 +1,14 @@
-f = open('../feature/9knife/1.txt')
+# -*- encoding: utf-8 -*-
 
-for line in f:
-	token = eval(line)
+import codecs
+
+import jieba as jb
+jb.load_userdict('../data/dict.txt')
+import jieba.posseg as ps
+
+if __name__ == '__main__':
+    tmp = u'我餓了'
+    xs = list(ps.cut(tmp.strip()))
+    with codecs.open('test.txt', 'w', encoding='utf8') as File:
+        for token in xs:
+            print >>File, token.word, token.flag
